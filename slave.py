14be55f266bdf4f18807760ecf9abbd07e861122
Márton Reiter <marci.reiter501@gmail.com>
2025-04-21 00:52:38 +0200

    Adding slave.py. Both scritps retrieve the same key.

--- slave.py
+++ slave.py
@@ -2,12 +2,11 @@ import requests
 import base64
 import urllib.parse
 
-MASTER_KMS_HOST = "" # Address of KMS, maybe there is only one
-MASTER_KMS_PORT = ""
-MASTER_KMS_BASE_URL = f"https://{MASTER_KMS_HOST}:{MASTER_KMS_PORT}/api/v1" # KMS cert might be self-signed?
-SLAVE_ID = ""
-KEY_LENGTH = 256  # In bits
-KEY_AMOUNT = 1
+SLAVE_KMS_HOST = "" # Address of KMS, maybe there is only one
+SLAVE_KMS_PORT = ""
+SLAVE_KMS_BASE_URL = f"https://{SLAVE_KMS_HOST}:{SLAVE_KMS_PORT}/api/v1" # KMS cert might be self-signed?
+MASTER_ID = ""
+KEY_ID = ""
 TIMEOUT = 10
 VERIFY_SSL = True # If KMS cert is self-signed, set to False, but be aware
 
@@ -17,20 +16,21 @@ VERIFY_SSL = True # If KMS cert is self-signed, set to False, but be aware
 #     "Authorization": f"Bearer {API_KEY}"
 # }
 
+def request_qkd_key_with_IDs() -> None:
+    get_key_with_IDs_url = f"{SLAVE_KMS_BASE_URL}/keys/{urllib.parse.urlencode(MASTER_ID)}/dec_keys"
 
-
-def request_qkd_key() -> None:
-    get_key_url = f"{MASTER_KMS_BASE_URL}/keys/{urllib.parse.urlencode(SLAVE_ID)}/enc_keys"
-
-    # Based on https://www.etsi.org/deliver/etsi_gs/QKD/001_099/014/01.01.01_60/gs_qkd014v010101p.pdf Table 10
+    # Based on https://www.etsi.org/deliver/etsi_gs/QKD/001_099/014/01.01.01_60/gs_qkd014v010101p.pdf Table 12
     payload = {
-        "number": KEY_AMOUNT,
-        "size": KEY_LENGTH,
+        "key_IDs": [
+            {
+                "key_ID": KEY_ID,
+            }
+        ],
     }
 
     try:
         response = requests.post(
-            url = get_key_url,
+            url = get_key_with_IDs_url,
             # headers = HEADERS,
             json = payload,
             timeout = TIMEOUT,
@@ -53,4 +53,4 @@ def request_qkd_key() -> None:
         print(f"❌ Request failed: {err}")
 
 if __name__ == "__main__":
-    request_qkd_key()
+    request_qkd_key_with_IDs()
